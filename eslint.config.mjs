a1674acc147b2f613685960781353f72e02663f9
mshnet7-cyber <msh.net7@gmail.com>
2026-09-16 12:24:32 +0400

    Fix ESLint flat config for pinned ESLint version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 
-export default defineConfig([
+export default [
   ...nextVitals,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+];
