92ac7c19c71e88393e012bf55eaa7b4a9e252934
mshnet7-cyber <msh.net7@gmail.com>
2026-09-18 05:46:09 +0400

    Stage 3 foundation

---
+++ app/PwaRegister.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function PwaRegister() {
+  useEffect(() => {
+    if (!("serviceWorker" in navigator)) return;
+    const id = window.setTimeout(() => {
+      navigator.serviceWorker.register("/sw.js", { scope: "/" }).catch(() => undefined);
+    }, 0);
+    return () => window.clearTimeout(id);
+  }, []);
+  return null;
+}
